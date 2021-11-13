064c28e83898bbd80df50366344b8398e02a45e4
Hasnaoui Dhekra <62170718+dhekra77@users.noreply.github.com>
2021-11-14 00:37:22 +0100

    Update IEntrepriseServiceTest.java

--- src/test/java/tn/esprit/spring/IEntrepriseServiceTest.java
+++ src/test/java/tn/esprit/spring/IEntrepriseServiceTest.java
@@ -28,7 +28,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class IEntrepriseServiceTest extends BaseJUnit49TestCase {
 	private static final Logger LOG = LogManager.getLogger(EntrepriseServiceImplTest.class);
-//hello
+
 	@Autowired
 	IEntrepriseService entServ;
 	@Autowired
