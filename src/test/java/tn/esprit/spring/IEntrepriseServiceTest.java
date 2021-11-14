fe3940624458c24ed745a0e6e0292af527dbd82c
Hasnaoui Dhekra <62170718+dhekra77@users.noreply.github.com>
2021-11-14 13:13:06 +0100

    Update IEntrepriseServiceTest.java

--- src/test/java/tn/esprit/spring/IEntrepriseServiceTest.java
+++ src/test/java/tn/esprit/spring/IEntrepriseServiceTest.java
@@ -93,7 +93,19 @@ public class IEntrepriseServiceTest extends BaseJUnit49TestCase {
 		}
 	}
     
-	
+	/*@Test
+	public void getAllEntrepriseTest() {
+	//	List<Entreprise> entreprises = entrepriseService.getAllEntreprise();
+		//LOG.log(Level.INFO, () -> "getAllEntreprise : " + entreprises);
+		//assertThat(entreprises.size()).isGreaterThan(0);
+    	
+    	List<Entreprise> entreprises = entrepriseService.getAllEntreprise();
+    	LOG.log(Level.INFO, () -> "getAllEntreprise : " + entreprises);
+    		assertThat(entreprises.get(1)).isEqualTo(entreprise.getName());
+    		//assertThat(names.get(1)).isEqualTo(employe2.getNom());
+    	}
+
+	*/
 	
 	
 
