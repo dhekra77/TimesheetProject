d6c66445a67ce4649894862c47715f5868220e0a
dhekra77 <dhekra.hasnaoui1@esprit.tn>
2021-11-15 08:39:54 +0100

    dhekre's

--- src/test/java/tn/esprit/spring/IEntrepriseServiceTest.java
+++ src/test/java/tn/esprit/spring/IEntrepriseServiceTest.java
@@ -28,7 +28,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class IEntrepriseServiceTest extends BaseJUnit49TestCase {
 	private static final Logger LOG = LogManager.getLogger(EntrepriseServiceImplTest.class);
-	
+	//helloo
 //test--webhook
 	@Autowired
 	IEntrepriseService entServ;
